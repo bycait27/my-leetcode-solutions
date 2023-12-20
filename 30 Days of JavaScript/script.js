032a2c43726711ce25908b02ee6b3a1307b5610f
Caitlin Ash <caitlinba27@gmail.com>
2023-12-20 17:18:03 -0600

    Added algorithm solution

--- 30 Days of JavaScript/script.js
+++ 30 Days of JavaScript/script.js
@@ -30,3 +30,35 @@ const counter = createCounter(10)
 counter() // 10
 counter() // 11
 counter() // 12
+
+// ------------------------------------------------------------------- 
+// To Be Or Not To Be Function
+
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+
+var expect = function(val) {
+    return {
+        toBe: function(val2) {
+            if (val === val2) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function(val2) {
+            if (val !== val2) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        },
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
