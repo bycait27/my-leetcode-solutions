9acd4c77e7435639374e5325389aa6a6c8b2fd12
Caitlin Ash <caitlinba27@gmail.com>
2023-12-20 17:18:59 -0600

    Added algorithm solution

--- 30 Days of JavaScript/script.js
+++ 30 Days of JavaScript/script.js
@@ -62,3 +62,36 @@ var expect = function(val) {
  * expect(5).toBe(5); // true
  * expect(5).notToBe(5); // throws "Equal"
  */
+
+// ------------------------------------------------------------------- 
+// Counter II Function
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+ 
+var createCounter = function(init) {
+    const resetInit = init;
+    return {
+        increment: () => {
+            return ++init;
+        },
+
+        decrement: () => {
+            return --init;
+        },
+
+        reset: () => {
+            init = resetInit;
+            return init;
+        },
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
